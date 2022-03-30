c85ae4d9d902e268edaee8e27777a33e8d75634e
AwsZFvR4Fh6 <102389487+AwsZFvR4Fh6@users.noreply.github.com>
2022-03-30 05:12:29 -0700

    Update KenzenGui.lua

--- KenzenGui.lua
+++ KenzenGui.lua
@@ -1,112 +1,133 @@
-local getgenv = getgenv and getgenv() or _G
-if getgenv.GuiLoaded then printconsole("Already loaded?") error("Already loaded?") return end
-local gui = game:GetObjects("rbxassetid://9013635661")[1]
-getgenv.GuiLoaded = true
+local Gui = game:GetObjects("rbxassetid://9234973824")[1]
+local DragFunc = loadstring(game:HttpGet("https://raw.githubusercontent.com/AwsZFvR4Fh6/Ya/main/drag.lua",true))()
 if not game:IsLoaded() then game.Loaded:Wait() end
-gui.Parent = game.CoreGui
-local script = gui.Frame.LocalScript
+local LoadTick = tick()
+
 local wait = task.wait
+local getgenv = getgenv and getgenv() or _G
 local TweenService = game:GetService("TweenService")
+local InputService = game:GetService("UserInputService")
+local Camera = workspace.Camera
+local Fullscreen = Gui.Fullscreen
 
-local fps = game.Stats.Workspace.FPS
+-- // Asset Library
+local Asset = {}
 
-local notify = loadstring(game:HttpGet("https://gist.githubusercontent.com/CenteredSniper/5aacd41c762304c92802ed4ad714cc98/raw/e8c9b25cb3660028793342728553efdb24e72bec/NotificationService.lua",true))()
-local function notification(title,duration) notify({Text = title,Duration = duration}) end
-
-local function dragify(Frame)
-	local frametomove = Frame
-	local dragToggle,dragInput,dragStart,startPos
-	local dragSpeed = 0
-	local function updateInput(input)
-		local Delta = input.Position - dragStart
-		--frametomove.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
-		local fpsval
-		pcall(function() fpsval = fps:GetValue() end)
-		fpsval = fpsval or 60
-		TweenService:Create(frametomove, TweenInfo.new(1/fpsval), {Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)}):Play()
+Asset.new = function(name,properties)
+	local NewAsset = Instance.new(name)
+	for i,v in pairs(properties) do
+		NewAsset[i] = v
 	end
-	Frame.InputBegan:Connect(function(input)
-		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and game:GetService("UserInputService"):GetFocusedTextBox() == nil then
-			dragToggle = true
-			dragStart = input.Position
-			startPos = frametomove.Position
-			input.Changed:Connect(function()
-				if input.UserInputState == Enum.UserInputState.End then
-					dragToggle = false
-				end
-			end)	
-		end
-	end)
-	Frame.InputChanged:Connect(function(input)
-		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-			dragInput = input
-		end
-	end)
-	game:GetService("UserInputService").InputChanged:Connect(function(input)
-		if input == dragInput and dragToggle then
-			updateInput(input)
-		end
+	return NewAsset
+end
+
+Asset.delay = function(asset,property,value,delaytime)
+	task.spawn(function() 
+		task.wait(delaytime)
+		asset[property] = value
 	end)
 end
 
-coroutine.resume(coroutine.create(function()
+Asset.spawn = function(funct)
+	return task.spawn(function() 
+		funct()
+	end)
+end
+
+-- // Main GUI
+
+local GuiIndex = {}
+local GuiToggle = false
+
+local Blur = Asset.new("BlurEffect",{["Size"]=12,["Enabled"]=false,["Name"]="KenzenBlur",["Parent"]=game.Lighting})
+
+InputService.InputBegan:connect(function(inputObject, gameProcessedEvent)
+	if inputObject.KeyCode == Enum.KeyCode.LeftBracket and not gameProcessedEvent then
+		GuiToggle = not GuiToggle
+		Blur.Enabled = GuiToggle
+		if GuiToggle then
+			Asset.delay(Fullscreen,"Visible",true,0)
+			local Fov = Camera.FieldOfView
+			TweenService:Create(Camera,TweenInfo.new(0.2,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),{FieldOfView = Fov+10}):Play()
+			for i,v in pairs(GuiIndex) do
+				if v:IsA("TextLabel") or v:IsA("TextButton") then
+					TweenService:Create(v,TweenInfo.new(0.15,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),{TextTransparency = v:GetAttribute("Transparency")}):Play()
+					if v:GetAttribute("BackgroundTransparency") then
+						TweenService:Create(v,TweenInfo.new(0.1,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),{TextTransparency = v:GetAttribute("Transparency")}):Play()
+					end
+				elseif v:IsA("ImageLabel") or v:IsA("ImageButton") then
+					TweenService:Create(v,TweenInfo.new(0.15,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),{ImageTransparency = v:GetAttribute("Transparency")}):Play()
+				else
+					TweenService:Create(v,TweenInfo.new(0.15,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),{BackgroundTransparency = v:GetAttribute("Transparency")}):Play()
+				end
+			end
+		else
+			Asset.delay(Fullscreen,"Visible",false,0.15)
+			local Fov = Camera.FieldOfView
+			TweenService:Create(Camera,TweenInfo.new(0.15,Enum.EasingStyle.Quad,Enum.EasingDirection.In),{FieldOfView = Fov-10}):Play()
+			for i,v in pairs(GuiIndex) do
+				if v:IsA("TextLabel") or v:IsA("TextButton") then
+					TweenService:Create(v,TweenInfo.new(0.1,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),{TextTransparency = 1}):Play()
+					if v:GetAttribute("BackgroundTransparency") then
+						TweenService:Create(v,TweenInfo.new(0.1,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),{BackgroundTransparency = 1}):Play()
+					end
+				elseif v:IsA("ImageLabel") or v:IsA("ImageButton") then
+					TweenService:Create(v,TweenInfo.new(0.1,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),{ImageTransparency = 1}):Play()
+				else
+					TweenService:Create(v,TweenInfo.new(0.1,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),{BackgroundTransparency = 1}):Play()
+				end
+			end
+		end
+	end 
+end)
+
+Asset.spawn(function()
 	wait(1)
-	TweenService:Create(script.Parent, TweenInfo.new(.5,Enum.EasingStyle.Quad,Enum.EasingDirection.In), {Size = UDim2.new(0,200,0,25)}):Play() -- resize
-	local icon = TweenService:Create(script.Parent.ImageLabel, TweenInfo.new(.5,Enum.EasingStyle.Quad,Enum.EasingDirection.In), {Size = UDim2.new(0,25,0,25)}) -- resize icon
-	icon:Play()
-	icon.Completed:Wait()
-	TweenService:Create(script.Parent.Frame, TweenInfo.new(.7,Enum.EasingStyle.Quad,Enum.EasingDirection.Out), {Size = UDim2.new(1,0,0,150)}):Play()
-	TweenService:Create(script.Parent.Minimize.TextButton, TweenInfo.new(1,Enum.EasingStyle.Quad,Enum.EasingDirection.In), {Position = UDim2.new(0,0,0,0)}):Play()
-	wait(0.2)
-	for i,v in pairs(script.Parent.Frame:GetChildren()) do
-		if v:IsA("TextButton") then
-			TweenService:Create(v, TweenInfo.new(.4+(v.Position.Y.Offset/200),Enum.EasingStyle.Quad,Enum.EasingDirection.Out), {Position = UDim2.new(0,0,0,v.Position.Y.Offset)}):Play()
-			if script.Parent.Parent:FindFirstChild(v.Name) then
-				v.Activated:Connect(function()
-					script.Parent.Parent[v.Name].Visible = not script.Parent.Parent[v.Name].Visible
-				end)
+	for i,v in pairs(Fullscreen:GetDescendants()) do
+		if v:IsA("GuiObject") then
+			if v.Name == "Drag" then
+				DragFunc(v,v.Parent)
+			else
+				table.insert(GuiIndex,v)
+				if not v:GetAttribute("Transparency") then
+					v:SetAttribute("Transparency",0)
+				end
 			end
 		end
 	end
-end))
-
-for i,v in pairs(script.Parent.Parent:GetChildren()) do
-	dragify(v)
-	v.Minimize.TextButton.Activated:Connect(function()
-		if v.Frame.Size.Y.Offset <= 3 then
-			TweenService:Create(v.Frame, TweenInfo.new(.3,Enum.EasingStyle.Quad,Enum.EasingDirection.Out), {Size = UDim2.new(1,0,0,v.FrameSize.Value)}):Play()
-		else
-			TweenService:Create(v.Frame, TweenInfo.new(.3,Enum.EasingStyle.Quad,Enum.EasingDirection.In), {Size = UDim2.new(1,0,0,0)}):Play()
-		end
-	end)
-	if v.Name ~= "Frame" then v.Visible = false end
-end
-
--- Kenzen
-script.Parent.Frame.Kenzen.Activated:Connect(function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/CenteredSniper/Kenzen/master/KenzenScript.lua",true))()
 end)
 
--- Reanimate
-script.Parent.Parent.Reanimate.Frame.TextButton.Activated:Connect(function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/CenteredSniper/Kenzen/master/newnetlessreanimate.lua",true))()
-end)
+-- // System Info
 
-for i,v in pairs(script.Parent.Parent.Reanimate.Frame.ScrollingFrame:GetChildren()) do
-	if v:IsA("Frame") then
-		v.TextLabel.Text = v.Name
-		v.ImageButton.Image = v.Activated.Value and "rbxassetid://4458804262" or "rbxassetid://4458801905"
-		v.ImageButton.Activated:Connect(function()
-			v.Activated.Value = not v.Activated.Value
-			v.ImageButton.Image = v.Activated.Value and "rbxassetid://4458804262" or "rbxassetid://4458801905"
-			getgenv[v.Name] = v.Activated.Value
-		end)
+Asset.spawn(function()
+	while wait(1) do
+		Fullscreen.SystemInfo.Ping.Text = math.round(game:GetService("Stats").PerformanceStats.Ping:GetValue()) .. "ms"
+		Fullscreen.SystemInfo.Memory.Text = game:GetService("Stats").PerformanceStats.Memory:GetValue() .. "mb"
 	end
-end
+end)
 
---Netless Scripts
+-- // Reanimate
 
-local netlessscripts = {
+local ReanimateVariables = {
+	["Fling"] = false,
+	["TorsoFling"] = false,
+	["ShowReal"] = false,
+	["GodMode"] = true,
+	["Tools"] = true,
+	["Collisions"] = true,
+	["Claim2"] = false,
+	["Notification"] = true,
+	["AutoAnimate"] = true,
+	["DynamicVelocity"] = false,
+	["Network"] = true,
+	["AntiSleep"] = false,
+	["Netless2"] = false,
+	["MovementVelocity"] = false,
+	["R6toR15"] = false,
+	["DynamicVelocityExperimental"] = false,
+}
+
+local ReanimateScripts = {
 	{"Star Platinum",function() 
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/AwsZFvR4Fh6/Ya/main/StarPlatinum.lua",true))()
 	end},
@@ -147,20 +168,42 @@ local netlessscripts = {
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/AwsZFvR4Fh6/Ya/main/eyozen.txt",true))()
 	end},
 }
-local scroll = script.Parent.Parent.Netless.Frame.ScrollingFrame
 
-for i,v in pairs(netlessscripts) do
-	local clone = scroll.Example:Clone()
-	clone.Name = v[1]
-	clone.TextLabel.Text = v[1]
-	clone.Parent = scroll
-	clone.Visible = true
-	clone.Activated:Connect(function()
-		v[2]()
-	end)
-end
+Asset.spawn(function()
+	local ReanimateFrame = Fullscreen.Reanimate.Frame.ScrollingFrame
+	for i,v in pairs(ReanimateVariables) do
+		local Clone = ReanimateFrame.ImageButton:Clone()
+		Clone.Visible = true
+		Clone.Name = i
+		Clone.TextLabel.Text = i
+		if not v then Clone.Selection.Visible = false end
+		Clone.Activated:Connect(function()
+			Clone.Selection.Visible = not Clone.Selection.Visible
+			getgenv[i] = Clone.Selection.Visible
+		end)
+		
+		Clone.Parent = ReanimateFrame
+	end
+end)
 
---Misc Scripts
+Asset.spawn(function()
+	local ReanimateScriptsFrame = Fullscreen.Reanimate.Scripts.ScrollingFrame
+	for i,v in pairs(ReanimateScripts) do
+		local Clone = ReanimateScriptsFrame.TextButton:Clone()
+		Clone.Visible = true
+		Clone.Name = v[1]
+		Clone.Text = v[1]
+		Clone.Activated:Connect(v[2])
+
+		Clone.Parent = ReanimateScriptsFrame
+	end
+end)
+
+Fullscreen.Reanimate.TextButton.Activated:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/CenteredSniper/Kenzen/master/newnetlessreanimate.lua",true))()
+end)
+
+-- // Script Hub
 
 local MiscScripts = {
 	{"Ping Predict",function() 
@@ -208,36 +251,54 @@ local MiscScripts = {
 	{"Antifling",function() 
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/L8X/phys/main/antifling.lua",true))()
 	end},
-}
-local scroll2 = script.Parent.Parent.Misc.Frame.ScrollingFrame
-
-for i,v in pairs(MiscScripts) do
-	local clone = scroll2.Example:Clone()
-	clone.Name = v[1]
-	clone.TextLabel.Text = v[1]
-	clone.Parent = scroll2
-	clone.Visible = true
-	clone.Activated:Connect(function()
-		v[2]()
-	end)
-end
-
--- AutoExec
-script.Parent.Frame.Autoexec.Activated:Connect(function()
-	if writefile then
-		if not isfolder("AutoexecFiles") then
-			makefolder("AutoexecFiles")
+	{"GetAutoExecute",function() 
+		if writefile then
+			if not isfolder("AutoexecFiles") then
+				makefolder("AutoexecFiles")
+			end
+			writefile("AutoexecFiles/one'sautoexec.txt",game:HttpGet("https://raw.githubusercontent.com/AwsZFvR4Fh6/Ya/main/extra/one'sautoexec.txt",true))
 		end
-		writefile("AutoexecFiles/one'sautoexec.txt",game:HttpGet("https://raw.githubusercontent.com/AwsZFvR4Fh6/Ya/main/extra/one'sautoexec.txt",true))
-		notification("Check workspace/AutoexecFiles",3)
-	else
-		notification("writefile not avaliable",3)
+	end},
+	{"KenzenScript",function() 
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/AwsZFvR4Fh6/Ya/main/KenzenScript.lua",true))()
+	end},
+}
+
+Asset.spawn(function()
+	local MiscScriptsFrame = Fullscreen.Scripts.Frame.ScrollingFrame
+	for i,v in pairs(ReanimateScripts) do
+		local Clone = MiscScriptsFrame.TextButton:Clone()
+		Clone.Visible = true
+		Clone.Name = v[1]
+		Clone.Text = v[1]
+		Clone.Activated:Connect(v[2])
+
+		Clone.Parent = MiscScriptsFrame
 	end
 end)
 
--- Notification
-script.Parent.Parent.Notification.Frame.TextButton.Activated:Connect(function()
-	if tonumber(script.Parent.Parent.Notification.Frame.Time.Text) then
-		notification(script.Parent.Parent.Notification.Frame.text.Text,tonumber(script.Parent.Parent.Notification.Frame.Time.Text))
-	end 
+Asset.spawn(function()
+	local function randomString()
+		local length = math.random(10,20)
+		local array = {}
+		for i = 1, length do
+			array[i] = string.char(math.random(32, 126))
+		end
+		return table.concat(array)
+	end
+
+	Gui.Name = randomString()
+	if syn then
+		syn.protect_gui(Gui)
+		Gui.Parent = game:GetService("CoreGui")
+	elseif get_hidden_gui or gethui then
+		local hiddenUI = get_hidden_gui or gethui
+		Gui.Parent = hiddenUI()
+	elseif game:GetService("CoreGui"):FindFirstChild('RobloxGui') then
+		Gui.Parent = game:GetService("CoreGui").RobloxGui
+	else
+		Gui.Parent = game:GetService("CoreGui")
+	end
 end)
+
+return LoadTick
