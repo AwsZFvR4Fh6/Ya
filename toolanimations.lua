426bd24e66370184d474567913ed63f757af6afa
AwsZFvR4Fh6 <102389487+AwsZFvR4Fh6@users.noreply.github.com>
2022-04-12 21:05:05 -0700

    Update toolanimations.lua

--- toolanimations.lua
+++ toolanimations.lua
@@ -1,65 +1,100 @@
 --local preloadanimations = true -- enable this if you want to preload all animations (lagspike)
-if not getgenv().preloadanimations then getgenv().preloadanimations = false end
-if not getgenv().loadtime then getgenv().loadtime = 0.1 end
-if not getgenv().reanimate then getgenv().reanimate = true end
+local function getsynassetfromurl(URL,Name)
+	if not isfolder("FakeAudios") then makefolder("FakeAudios") end
+	Name = "FakeAudios/" .. Name
+	local getsynasset, request = getsynasset or getcustomasset or error('invalid attempt to \'getsynassetfromurl\' (custom asset retrieval function expected)'), (syn and syn.request) or (http and http.request) or (request) or error('invalid attempt to \'getsynassetfromurl\' (http request function expected)')
+	if isfile(Name .. ".ogg") then
+		return getsynasset(Name .. ".ogg")
+	else
+		local Extension, Types, URL = '', {'.png', '.webm'}, assert(tostring(type(URL)) == 'string', 'invalid argument #1 to \'getsynassetfromurl\' (string [URL] expected, got '..tostring(type(URL))..')') and URL or nil
+		local Response, TempFile = request({
+			Url = URL,
+			Method = 'GET'
+		})
 
-local wait = getgenv().MiliWait and getgenv().MiliWait.Event or game:GetService("RunService").Heartbeat --task.wait
+		if Response.StatusCode == 200 then
+			Extension = Response.Body:sub(2, 4) == 'PNG' and '.png' or Response.Body:sub(25, 28) == 'webm' and '.webm' or nil
+		end
 
-local files = game:GetObjects("rbxassetid://9136313101")[1]
-if getgenv().preloadanimations then
-    local gui = files.ScreenGui:Clone()
-    gui.Parent = game.CoreGui
-    local loadamount,amounttoload = 0,0
-    amounttoload = #files.Folder:GetChildren()--amounttoload + 1
-    for i,v in pairs(files.Folder:GetChildren()) do
-        if getgenv().loadtime ~= 0 then
-            wait:Wait(getgenv().loadtime)
-        end
-        spawn(function()
-            local animid,soundid = v.ToolTip,v.SoundID.SoundId
-            if soundid then
-                local soundwait = Instance.new("Sound",game.Players.LocalPlayer)
-                soundwait.SoundId = soundid
-                spawn(function()
-                        soundwait.Loaded:Wait()
-                        soundwait:Destroy()    
-                end)
-            end
-            if animid then
-                pcall(function()
-                    game:GetObjects('rbxassetid://'..animid)
-                end)
-            end
-            
-            loadamount = loadamount + 1
-            print(loadamount,amounttoload)
-        end)
-    end
-    repeat wait:Wait() until loadamount == amounttoload
-    gui:Destroy()
-end
-if getgenv().reanimate then
-  getgenv().AutoAnimate = false
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/CenteredSniper/Kenzen/master/newnetlessreanimate.lua",true))()
-   wait = getgenv().MiliWait.Event
-  wait:Wait()
+		if Response.StatusCode == 200 then--and (Extension and table.find(Types, Extension)) then
+			for i = 1, 15 do
+				local Letter, Lower = string.char(math.random(65, 90)), math.random(1, 5) == 3 and true or false
+			end
+
+			writefile(Name..".ogg", Response.Body)
+
+			return getsynasset(Name..".ogg")
+		elseif Response.StatusCode ~= 200 or not Extension then
+			warn('unexpected \'getsynassetfromurl\' Status Error: ' .. Response.StatusMessage .. ' ('..URL..')')
+		elseif not (Extension) then
+			warn('unexpected \'getsynassetfromurl\' Error: (PNG or webm file expected)')
+		end
+	end
 end
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AwsZFvR4Fh6/Ya/main/AnimyForthing",true))()
+if getgenv().Preload == nil then getgenv().Preload = false end
+if getgenv().PreloadWait == nil then getgenv().PreloadWait = 0.1 end
+if getgenv().Reanimate == nil then getgenv().Reanimate = true end
 
-for i,v in pairs(files.Folder:GetChildren()) do
-    local tool = v --:Clone()
-        tool.Parent = game.Players.LocalPlayer.Backpack
-        wait:Wait()
-        tool.Activated:Connect(function()
-            if _G.runanimation then
-                _G.runanimation(v.ToolTip,string.sub(v.SoundID.SoundId,14))
-            end
-        end)
-        tool.Unequipped:Connect(function()
-            _G.dancing = false
-            if game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild("Sound") then
-                game.Players.LocalPlayer.Character.HumanoidRootPart.Sound:Destroy()
-            end
-        end)
+local Files = game:GetObjects("rbxassetid://9353862873")[1]
+if getgenv().Preload then
+	local GUI = Files.ScreenGui:Clone()
+	GUI.Parent = game.CoreGui
+	local LoadAmount,NumberToLoad = 0,#Files.Folder:GetChildren()
+	for i,v in pairs(Files.Folder:GetChildren()) do
+		if getgenv().PreloadWait > (1/60) then
+			task.wait(getgenv().PreloadWait)
+		end
+		task.spawn(function()
+			local AnimationID,soundid = v.ToolTip,v.SoundID.SoundId
+			if soundid then
+				local soundwait = Instance.new("Sound",game.Players.LocalPlayer)
+				soundwait.SoundId = v:FindFirstChild("FakeAsset") and getsynassetfromurl(v.FakeAsset.Value,v.ToolTip) or v.SoundID.SoundId
+				task.spawn(function()
+					soundwait.Loaded:Wait()
+					soundwait:Destroy()    
+				end)
+			end
+			if AnimationID then
+				pcall(function()
+					game:GetObjects('rbxassetid://'..AnimationID)
+				end)
+			end
+
+			loadamount = loadamount + 1
+			print(loadamount,NumberToLoad)
+		end)
+	end
+	repeat task.wait(0/1) until loadamount == NumberToLoad
+	GUI:Destroy()
+end
+
+if getgenv().Reanimate then
+	getgenv().AutoAnimate = false
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/CenteredSniper/Kenzen/master/newnetlessreanimate.lua",true))()
+	task.wait(0/1)
+end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AwsZFvR4Fh6/Ya/main/AnimZ.lua",true))()
+
+for i,v in pairs(Files.Folder:GetChildren()) do
+	task.spawn(function()
+		local tool = v --:Clone()
+		tool.Parent = game.Players.LocalPlayer.Backpack
+		task.wait(0/1)
+		local ToolPlaying = false
+		tool.Activated:Connect(function()
+			if getgenv().RunAnimation then
+				local SoundID = v:FindFirstChild("FakeAsset") and getsynassetfromurl(v.FakeAsset.Value,v.ToolTip) or v.SoundID.SoundId
+				ToolPlaying = true
+				getgenv().RunAnimation(v.ToolTip,SoundID)
+			end
+		end)
+		tool.Unequipped:Connect(function()
+			if ToolPlaying then
+				ToolPlaying = false
+				getgenv().RunAnimation()
+			end
+		end)
+	end)
 end
